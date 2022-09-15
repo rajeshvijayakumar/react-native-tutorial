e3b82c953b7d18d79c04ccb405aaa3f3f05998e9
Rajesh Vj <rajeshvj.2412@gmail.com>
2022-09-15 16:54:21 +0530

    FlatList Example added.

--- App.js
+++ App.js
@@ -8,80 +8,81 @@
 
 import React, { useState } from 'react';
 
-import { ScrollView, View, Text, StyleSheet, RefreshControl, FlatList, SectionList } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, RefreshControl, FlatList } from 'react-native';
 
 
 const App = () => {
-  
-  const [Sections, setSections] = useState([
-    {
-      title: "Title 1",
-      data: ['Item 1-1', 'Item 1-2']
-    }
+  const [Items, setItems] = useState([
+    { name: "Item 1" },
+    { name: "Item 2" },
+    { name: "Item 3" },
+    { name: "Item 4" },
+    { name: "Item 5" },
+    { name: "Item 6" },
+    { name: "Item 7" },
+    { name: "Item 8" },
+    { name: "Item 9" },
+    { name: "Item 10" },
+    { name: "Item 11" },
+    { name: "Item 12" },
   ])
 
   const [Refreshing, setRefreshing] = useState(false);
   const onRefresh = () => {
     setRefreshing(true);
-    const addingIndex = Sections.length + 1;
-
-    setSections([...Sections,
-      {
-        title: 'Title ' + addingIndex,
-        data: [
-          'Item ' + addingIndex + '-1',
-          'Item ' + addingIndex + '-2',
-        ]
-      }
-    ])
+    setItems([...Items, { name: "Item 13" }]);
     setRefreshing(false);
   }
   // horizontal={true}
   // inverted={true}
   return (
-    <SectionList
+    <FlatList
       keyExtractor={(item, index) => index.toString()}
-      sections={Sections}
-      renderItem={({item}) => (
+      data={Items}
+      renderItem={({ item }) => (
         <View style={styles.item}>
-        <Text style={styles.text_item}>{item}</Text>
-      </View >
-      )}
-      renderSectionHeader={({ section }) => (
-        <View style={styles.header}>
-        <Text style={styles.text_header}>{section.title}</Text>
-      </View>
-      )}
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )
+      }
       refreshControl={
         <RefreshControl refreshing={Refreshing}
           onRefresh={onRefresh} />
-      }
-    />
+      } />
+    // <ScrollView refreshControl={
+    //   <RefreshControl refreshing={Refreshing}
+    //     onRefresh={onRefresh} />
+    // } style={styles.body}>
+    //   {
+    //     Items.map((itemObj) => {
+    //       return (
+    //         <View style={styles.item} key={itemObj.key}>
+    //           <Text style={styles.text}>{itemObj.item}</Text>
+    //         </View>
+    //       )
+    //     })
+    //   }
+    // </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
+  body: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+  },
+  item: {
+    margin: 10,
     backgroundColor: '#4ae1fa',
     justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 2,
   },
-  item: {
-    borderBottomWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text_header: {
+  text: {
     color: '#000000',
     fontSize: 45,
     fontStyle: 'italic',
     margin: 10,
-  },
-  text_item: {
-    color: '#000000',
-    fontSize: 35,
-    margin: 5,
   },
 });
 
